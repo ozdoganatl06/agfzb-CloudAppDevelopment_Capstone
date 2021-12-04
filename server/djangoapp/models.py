8e36f1775e34f9b86f810d56d6d7969f637af9bf
ozdoganatl06 <ozdoganatl06@gmail.com>
2021-12-04 14:57:07 +0100

    initial commit

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+import sys
 from django.utils.timezone import now
+try:
+    from django.db import models
+except Exception:
+    print("There was an error loading django modules. Do you have django installed?")
+    sys.exit()
+
+from django.conf import settings
+import uuid
 
 
 # Create your models here.
@@ -9,7 +18,52 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+# Lesson model
+class CarMake(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20, default="name")
+    description =models.CharField(max_length=20, default="description")
 
+
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
+  
+class CarModel(models.Model):
+    modelle = models.CharField(max_length=200, default="modelle")
+    modelle = models.ManyToManyField(CarMake)
+    modelle = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id= models.AutoField(primary_key=True)
+    name =models.CharField(max_length=20, default="name")
+ 
+    
+    RED = 'Red'
+    BLUE = 'Blue'
+    YELLOW = 'Yellow'
+    WHITE= 'White'
+    BLACK= 'Black'
+    COLOR_CHOICES = [
+        (RED, 'Red'),
+        (BLUE, 'Blue'),
+        (YELLOW, 'Yellow'),
+        (WHITE,'White'),
+        (BLACK,'Black')
+        ]
+    color = models.CharField(max_length=6, choices=COLOR_CHOICES, default=WHITE)
+    year =  models.DateField()
+
+    SUV = 'Suv'
+    SEDAN = 'Sedan'
+    WAGON = 'Wagon'
+    TYPE_CHOICES = [
+        (RED, 'Suv'),
+        (BLUE, 'Sedan'),
+        (YELLOW, 'wagon')
+    ]
+    type = models.CharField(max_length=6, choices=TYPE_CHOICES, default=SUV)
+
+    def __str__(self):
+        return "Name: " + self.name 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ["ozdogan.eu-gb.mybluemix.net"]
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ["ozdogan.eu-gb.mybluemix.net"]
 
 # Application definition
 
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
